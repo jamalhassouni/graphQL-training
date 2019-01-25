7cfc459ed7654ecece565d62ee43a6c6026fa894
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-01-25 21:56:02 +0100

    Create Book Details Component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {graphql} from "react-apollo";
 import {getBooksQuery} from "../queries/queries";
 
+// components
+import BookDetails from "./BookDetails";
 
 
 class BookList extends Component {
@@ -25,6 +27,7 @@ class BookList extends Component {
      <ul id="book-list">
      	{this.displayBooks()}
      </ul>
+     <BookDetails />
      </div>
     );
   }
