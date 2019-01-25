fb21f63d4ac3cc74086bbecfea87e956623543da
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-01-25 19:03:03 +0100

    Making Queries From React

--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const graphQLHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose  = require("mongoose");
+const cors = require("cors");
 
 const app  = express();
+
+// allow cross-origin requests
+
+app.use(cors());
 
 // connect to mongodb database
 
