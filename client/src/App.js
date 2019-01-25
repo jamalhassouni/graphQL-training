c8471533f9c86ef7f547197b70e7934361132382
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-01-25 19:30:53 +0100

    Create AddBook Component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {ApolloProvider} from "react-apollo";
 
 // components
 import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 // apollo client setup 
 
@@ -19,6 +20,7 @@ class App extends Component {
       <div id="main">
         <h1>Reading List</h1>
         <BookList/>
+        <AddBook/>
       </div>
       </ApolloProvider>
     );
