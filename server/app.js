60e91558fb0b60bd556719043a945d09c090bbc8
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-01-25 16:45:05 +0100

    Connecting to mongodb database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const graphQLHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose  = require("mongoose");
 
 const app  = express();
+
+// connect to mongodb database
+
+mongoose.connect("mongodb://localhost:27017/graphql");
+
+mongoose.connection.once('open',() => {
+	console.log("connected to database");
+});
+
 
 app.use("/graphql",graphQLHTTP({
  schema,
